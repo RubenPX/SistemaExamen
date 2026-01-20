9f5fc08c99570bdb174c47ebb27635f4c2395e02
RubenPX <rubenmarchante@gmail.com>
2026-01-20 15:06:17 +0100

    No inicia oracle al principio para evitar cargas lentas

--- SistemaExamen.AppHost/Program.cs
+++ SistemaExamen.AppHost/Program.cs
@@ -1,5 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Base de datos final
+var oracle = builder.AddOracle("oracle")
+                    .WithDataVolume()
+                    .WithLifetime(ContainerLifetime.Persistent)
+                    .WithExplicitStart();
+
+// Cache Redis
 var redis = builder.AddRedis("redis")
                 .WithDataVolume()
                 .WithPersistence(TimeSpan.FromSeconds(1)); // Activa RDB/AOF
@@ -9,19 +16,19 @@ builder.AddContainer("redis-insight", "redislabs/redisinsight")
        .WithReference(redis)
        .WithLifetime(ContainerLifetime.Persistent);
 
-var oracle = builder.AddOracle("oracle")
-                    .WithDataVolume()
-                    .WithLifetime(ContainerLifetime.Persistent);
-
+// Servidor
 var server = builder.AddProject<Projects.Examenes_Server>("server")
+    .WithReference(oracle)
     .WaitFor(redis).WithReference(redis)
-    .WaitFor(oracle).WithReference(oracle)
     .WithHttpCommand("/api/finalizarexamen", "Migrar datos a oracle", commandOptions: new() {
         Method = HttpMethod.Get,
         ConfirmationMessage = "Quieres que se inicie la exportación?",
     });
 
+// Simulador
 builder.AddProject<Projects.Examenes_Simulator>("simulator")
+    .WithEnvironment("MAX_CONNECTIONS", "500")
+    .WithEnvironment("MAX_EVENTS", "1_000_000")
     .WithReference(server)
     .WithExplicitStart();
 
