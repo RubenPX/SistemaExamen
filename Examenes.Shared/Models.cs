19ddfe335f11f5a528f8a7543e318c785f6ecaed
RubenPX <rubenmarchante@gmail.com>
2026-01-20 14:53:51 +0100

    Lint

--- Examenes.Shared/Models.cs
+++ Examenes.Shared/Models.cs
@@ -1,8 +1,10 @@
-using System.Text.Json.Serialization;
-
 namespace Examenes.Domain;
 
-public enum TipoAccion : byte { CargaExamen = 1, MarcaPregunta = 2, FinalizaExamen = 3 }
+public enum TipoAccion : byte {
+    CargaExamen = 1,
+    MarcaPregunta = 2,
+    FinalizaExamen = 3
+}
 
 // Usamos Record para inmutabilidad y facilidad de transporte
 public readonly record struct AccionEvento(
